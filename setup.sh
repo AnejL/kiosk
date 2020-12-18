eec9908cf7628e69e7608443ca2dd682a6c3bd18
anejl <anej.lekse@gmail.com>
2020-12-18 10:40:33 +0100

    fix screen

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 # $1 = line, $2 = file
 function lineinfile {
 	present=$(cat $2 | grep $1 | wc -l)
-	if [ present -eq 0 ]; then
+	
+	if [ $present -eq 0 ]; then
 		echo $1 >> $2
 	fi
 }
@@ -13,7 +14,7 @@ function lineinfile {
 # $1 = line, $2 = file
 function sudolineinfile {
 	present=$(sudo cat $2 | grep $1 | wc -l)
-	if [ present -eq 0 ]; then
+	if [ $present -eq 0 ]; then
 		sudo echo $1 >> $2
 	fi
 }
@@ -23,8 +24,10 @@ sudo apt-get install --no-install-recommends xserver-xorg-video-all xserver-xorg
 
 echo "Screen rotation setup"
 sudolineinfile "display_rotate=1" "/boot/config.txt"
+# sudolineinfile "display_rotate=1" "test.txt"
 
 echo "Making config directory and copying over the touchscreen config"
 [ ! -d "/etc/X11/xorg.conf.d" ] && sudo mkdir /etc/X11/xorg.conf.d
 sudo cp 40-libinput.conf /etc/X11/xorg.conf.d/
+
 
